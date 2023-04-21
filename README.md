891499b3291c530b8321bc66170a6c7579800f01
Tautvydas <tassat12321@gmail.com>
2023-04-21 19:15:24 +0300

    Version 1.1.0
    Loading, editing, better error messages

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 # Wrestling Empire Custom Promo Generator
 ## Usage
-### Automatic installation
-After the mod manager installs the tool, click `Browse profile folder`, go to `GeeEm-PromoGenerator-1.0.0/plugins/Assets/` and launch `WECustomPromoGenerator.exe`. Follow the instructions on the screen. When you are done making pages click `Create .promo file` and the tool should make a new .promo file. It *should* appear in game the next time you launch it.
-### Manual installation
-Download the zip and extract it. Go to `/plugins/Assets/` and launch `WECustomPromoGenerator.exe`. Follow the instructions on the screen. When you are done making pages click `Create .promo file` and the tool should make a new .promo file. Put that file in your `<Game directory>\BepInEx\plugins\Assets` folder and it should appear in game the next time you launch it.
+### Automatic Installation
+After the mod manager installs the tool, click `Browse profile folder`, go to `\BepInEx\plugins\GeeEm-PromoGenerator\Assets` and launch `WECustomPromoGenerator.exe`. Follow the instructions on the screen. When you are done making pages click `Create .promo file` and the tool should make a new .promo file. It *should* appear in game the next time you launch it.
+### Manual Installation
+Download the zip and extract it. Go to `\plugins\Assets\` and launch `WECustomPromoGenerator.exe`. Follow the instructions on the screen. When you are done making pages click `Create .promo file` and the tool should make a new .promo file. Put that file in your `<Game directory>\BepInEx\plugins\Assets` folder and it should appear in game the next time you launch it.
 ### Instructions
+Example: https://imgur.com/a/0aIh4XM
+- `Load .promo file` - you can load previously created .promo file for editing. 
 - `Please enter the promo title:` - the title of the custom promo. The tool will also use this as the file name.
 - `Please enter the promo description: (You can use '[P1]' and '[P2]' to name wrestlers)` - the promo description. [P1] and [P2] will be replaced with the names of the characters.
-- `How many speakers are there? Min 0, max 3, not counting the ref or team partners:` - how many main wrestler speakers are there.
-- `Is the wrestler 1 team partner included in the promo?` - adds wrestler 1 team partner to the promo. There should be at least 1 main wrestler to work correctly.
-- `Is the wrestler 2 team partner included in the promo?` - adds wrestler 2 team partner to the promo. There should be at least 2 main wrestlers to work correctly.
+- `How many speakers are there? Min 0, max 3, not counting the ref or team partners:` - how many main wrestler speakers are there. 
+- `Is the wrestler 1 team partner included in the promo?` - adds wrestler 1 team partner to the promo. There should be at least 1 main wrestler to work correctly. Team format must be set in the match rules.
+- `Is the wrestler 2 team partner included in the promo?` - adds wrestler 2 team partner to the promo. There should be at least 2 main wrestlers to work correctly. Team format must be set in the match rules.
 - `Is the ref included in the promo?` - adds ref to the promo.
-- `Next step` - will move to promo page generation.
+- `Update id list` - will display the wrestler ids you can use. Make sure to always update and check the ids after you make changes to previous fields.
 - `Create new page` - creates new promo page:
-     - `Enter the first line of the speaker:` / `Enter the second line of the speaker:` - each promo page can have up to 2 lines. For quotes, use `\"`. `$name#` will be replaced with the name of the character with the corresponding id. `@him/his/etc.#` will be replaced with the pronoun of the character with the corresponding id, e.g. `@his1 friend` -> `his friend` or `her friend` depending on wrestler #1's gender. Supported pronouns are `He, he, His, his, Male, male, Man, man, Guy, guy, Boy, boy`.
+     - `Enter the first line of the speaker:` / `Enter the second line of the speaker:` - each promo page can have up to 2 lines. For quotes, use `\"`. In order to use wrestler names use `$name#` and replace `#` with the corresponding wrestler id, e.g. if you type `I am $name1` and assign Whack Ax as the first wrestler in the game, he will say `I am Whack Ax`. `@him/his/etc.#` will be replaced with the pronoun of the character with the corresponding id, e.g. `@his1 friend` -> `his friend` or `her friend` depending on wrestler #1's gender. Supported pronouns are `He, he, His, his, Him, him, Male, male, Man, man, Guy, guy, Boy, boy`.
      - `Enter the speaker id:` / `Enter the receiver id:` - use the provided character ids.
      - `Enter the taunt number (0 for none):` - list of taunts can be found here: https://github.com/IngoHHacks/WECCL/blob/main/TauntAnims.md
      - `Is the speaker happy (H) or angry (A)? (enter 0 for none):` - enter `H` to make the character happy, `A` to make them angry, `0` for none.
+	 - `Delete this page` - you can delete the page if you don't need it.
 - `Create .promo file` - will generate the .promo file. Click this once you are done making the custom promo.
-### Manual .promo editing
+### Manual .promo Editing
 .promo files can be opened and edited in notepad. For more information about how to edit them manually read the [WECCL documentation](https://thunderstore.io/c/wrestling-empire/p/IngoH/WECCL/).
+## Special Thanks
+IngoH for creating WECCL and letting this come to life.
--- WECustomPromoGenerator.py
+++ WECustomPromoGenerator.py
@@ -1,18 +1,233 @@
 import tkinter as tk
+import re
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+from tkinter.messagebox import showerror
 import traceback
 
-title = ""
-description = ""
-wrestlers = -1
-total = -1
-team1 = -1
-team2 = -1
-ref = -1
-baseInfo = []
 pages = []
 
-def formatPromo(button1, button2):
+def loadPromo():
+    filetypes = (
+        ('.promo files', '*.promo'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='.',
+        filetypes=filetypes)
     try:
+        f = open(filename, "r")
+        lines = f.readlines()
+        f.close()
+        titleEntry.delete(0, tk.END)
+        titleEntry.insert(-1, lines[0][7:].strip())
+        descEntry.delete(0, tk.END)
+        descEntry.insert(-1, lines[1][13:].strip())
+        wrestlersLine = lines[2][12:].strip().split(',')
+        wrestlers = 0
+        total = 0
+        team1 = 0
+        team2 = 0
+        ref = 0
+        if(len(wrestlersLine) == 1):
+            if(wrestlersLine == "-1"):
+                ref = 1
+            else:
+                wrestlers = wrestlersLine[0]
+        else:
+            for x in wrestlersLine:
+                if (x == "1" or x == "2" or x == "3"):
+                    wrestlers += 1
+                if (x == "11"):
+                    team1 = 1
+                if (x == "22"):
+                    team2 = 1
+                if (x == "-1"):
+                    ref = 1
+        wrestlersEntry.delete(0, tk.END)
+        wrestlersEntry.insert(-1, str(wrestlers))
+        team1Var.set(team1)
+        team2Var.set(team2)
+        refVar.set(ref)
+        updateID()
+        for x in pages:
+            x[0].master.destroy()
+        pages[:] = []
+        i = 0
+        for x in lines[3:]:
+            newPage()
+            pageLine = re.split(r',(?=(?:[^"]|"[^"\\]*(?:\\\\.[^"\\]*)*")*$)', x) #regex source: chatgpt
+            line1 = pageLine[0][1:-1]
+            line2 = pageLine[1][1:-1]
+            speaker = pageLine[2]
+            target = pageLine[3]
+            taunt = "0"
+            if(len(pageLine) > 4):
+                taunt = pageLine[4]
+            demeanor = "0"
+            if(len(pageLine) > 5):
+                demeanor = pageLine[5]
+                d = int(demeanor)
+                if(d > 0):
+                    demeanor = "H"
+                if(d < 0):
+                    demeanor = "A"
+            pages[i][0].insert(-1, line1)
+            pages[i][1].insert(-1, line2)
+            pages[i][2].insert(-1, speaker)
+            pages[i][3].insert(-1, target)
+            pages[i][4].insert(-1, taunt)
+            pages[i][5].insert(-1, demeanor)
+            i+=1
+    except:
+        traceback.print_exc()
+        showerror(title='Wrestling Empire Custom Promo Generator', message="Failed to load the file.")
+
+def onFrameConfigure(canvas):
+    '''Reset the scroll region to encompass the inner frame'''
+    canvas.configure(scrollregion=canvas.bbox("all"))
+
+root = tk.Tk()
+root.title("Wrestling Empire Custom Promo Generator")
+root.geometry("600x500")  #Window size is messed up after compiling, dunno why
+canvas = tk.Canvas(root, borderwidth=0)
+frame = tk.Frame(canvas)
+vsb = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
+canvas.configure(yscrollcommand=vsb.set)
+
+vsb.pack(side="right", fill="y")
+canvas.pack(side="left", fill="both", expand=True)
+canvas.create_window((4,4), window=frame, anchor="nw")
+
+frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+
+versionLabel = tk.Label(frame,text="Version: 1.1.0")
+versionLabel.grid()
+loadPromoButton = tk.Button(frame, text="Load .promo file", command=loadPromo)
+loadPromoButton.grid()
+titleLabel = tk.Label(frame,text="Please enter the promo title:")
+titleLabel.grid()
+titleEntry = tk.Entry(frame)
+titleEntry.grid()
+descLabel = tk.Label(frame,text="Please enter the promo description: (You can use '[P1]' and '[P2]' to name wrestlers)")
+descLabel.grid()
+descEntry = tk.Entry(frame,width=100)
+descEntry.grid()
+wrestlersLabel = tk.Label(frame,text="How many speakers are there? Min 0, max 3, not counting the ref or team partners:")
+wrestlersLabel.grid()
+wrestlersEntry = tk.Entry(master=frame)
+wrestlersEntry.grid()
+
+team1Var = tk.IntVar()
+team2Var = tk.IntVar()
+refVar = tk.IntVar()
+team1Check = tk.Checkbutton(frame,text="Is the wrestler 1 team partner included in the promo?", variable=team1Var, onvalue=1, offvalue=0)
+team1Check.grid()
+team2Check = tk.Checkbutton(frame,text="Is the wrestler 2 team partner included in the promo?", variable=team2Var, onvalue=1, offvalue=0)
+team2Check.grid()
+refCheck = tk.Checkbutton(frame,text="Is the ref included in the promo?", variable=refVar, onvalue=1, offvalue=0)
+refCheck.grid()
+
+nextStep = tk.Button(frame, text="Update id list", command=lambda:[updateID()])
+nextStep.grid()
+idFrame = tk.Frame(frame)
+idFrame.grid()
+newPageButton = tk.Button(frame, text="Create new page", command=lambda:[newPage()])
+createPromoButton = tk.Button(frame, text="Create .promo file", command=lambda:[formatPromo()])
+newPageButton.grid()
+createPromoButton.grid()
+
+def updateID():
+    for widgets in idFrame.winfo_children():
+      widgets.destroy()
+    try:
+        wrestlers = int(wrestlersEntry.get())
+        team1 = team1Var.get()
+        team2 = team2Var.get()
+        ref = refVar.get()
+        
+        for i in range(wrestlers):
+            tk.Label(idFrame,text=f"Wrestler {i+1} id: {i+1}").grid()
+        if team1 == 1:
+            tk.Label(idFrame,text=f"Wrestler 1 team member id: {wrestlers + team1}").grid()
+        if team2 == 1:
+            tk.Label(idFrame,text=f"Wrestler 2 team member id: {wrestlers + team1 + team2}").grid()
+        if ref == 1:
+            tk.Label(idFrame,text=f"Ref id: {wrestlers + team1 + team2 + ref}").grid()
+        
+    except:
+        traceback.print_exc()
+        showerror(title='Wrestling Empire Custom Promo Generator', message="An error has occured, check if everything is correct and try again!")
+
+def newPage():
+    pageInfo = []
+    newPageframe = tk.Frame(frame)
+    line1Label = tk.Label(newPageframe, text="Enter the first line of the speaker:")
+    line1Label.grid()
+    line1Entry = tk.Entry(newPageframe, width=100)
+    line1Entry.grid()
+    pageInfo.append(line1Entry)
+    
+    line2Label = tk.Label(newPageframe, text="Enter the second line of the speaker:")
+    line2Label.grid()
+    line2Entry = tk.Entry(newPageframe, width=100)
+    line2Entry.grid()
+    pageInfo.append(line2Entry)
+    
+    speakerLabel = tk.Label(newPageframe, text="Enter the speaker id:")
+    speakerLabel.grid()
+    speakerEntry = tk.Entry(newPageframe)
+    speakerEntry.grid()
+    pageInfo.append(speakerEntry)
+    
+    receiverLabel = tk.Label(newPageframe, text="Enter the receiver id:")
+    receiverLabel.grid()
+    receiverEntry = tk.Entry(newPageframe)
+    receiverEntry.grid()
+    pageInfo.append(receiverEntry)
+    
+    tauntLabel = tk.Label(newPageframe, text="Enter the taunt number (0 for none):")
+    tauntLabel.grid()
+    tauntEntry = tk.Entry(newPageframe)
+    tauntEntry.grid()
+    pageInfo.append(tauntEntry)
+    
+    demeanorLabel = tk.Label(newPageframe, text="Is the speaker happy (H) or angry (A)? (enter 0 for none): ")
+    demeanorLabel.grid()
+    demeanorEntry = tk.Entry(newPageframe)
+    demeanorEntry.grid()
+    pageInfo.append(demeanorEntry)
+    pages.append(pageInfo)
+    
+    deletePageButton = tk.Button(newPageframe, text="Delete this page", command=lambda:[pages.remove(pageInfo),newPageframe.destroy()])
+    deletePageButton.grid()
+    separatorLabel = tk.Label(newPageframe, text="--------------------------------------------------------------------------------------------------")
+    separatorLabel.grid()
+
+    newPageframe.grid()
+
+def formatPromo():
+    try:
+        title = titleEntry.get()
+        if title == "":
+            raise Exception("No title")
+        description = descEntry.get()
+        if description == "":
+            raise Exception("No description")
+        if wrestlersEntry.get() == "":
+            raise Exception("No wrestlers")
+        wrestlers = int(wrestlersEntry.get())
+        team1 = team1Var.get()
+        team2 = team2Var.get()
+        ref = refVar.get()
+        total = wrestlers + team1 + team2 + ref
+        if len(pages) == 0:
+            raise Exception("No pages")
+        for x in pages:
+            if (x[2].get() == "" or x[3].get() == ""):
+                raise Exception("No speaker/target")
         filename = title + ".promo"
         f = open(filename, "w")
         f.write("title: " + title + "\n")
@@ -42,173 +257,10 @@ def formatPromo(button1, button2):
             strline += str(d)
             f.write(strline + "\n")
         f.close()
-        for x in pages:
-            for i in range(6):
-                x[i].config(state="disabled")
-        button1.grid_forget()
-        button2.grid_forget()
-        wd = tk.Tk()
-        wd.title("Wrestling Empire Custom Promo Generator")
-        label = tk.Label(wd, text=".promo file was successfully generated! You can now close this tool")
-        label.pack()
+        showinfo(title='Wrestling Empire Custom Promo Generator', message=".promo file was successfully generated! You can now close this tool")
     except:
         traceback.print_exc()
-        wd = tk.Tk()
-        wd.title("Wrestling Empire Custom Promo Generator")
-        label = tk.Label(wd, text="An error has occured, check if everything is correct and try again!")
-        label.pack()
+        showerror(title='Wrestling Empire Custom Promo Generator', message="An error has occured, check if everything is correct and try again!")
 
-def newPage(frame):
-    pageInfo = []
-    newPagecanvas = tk.Canvas(frame)
-    line1Label = tk.Label(newPagecanvas, text="Enter the first line of the speaker:")
-    line1Label.grid()
-    line1Entry = tk.Entry(newPagecanvas, width=100)
-    line1Entry.grid()
-    pageInfo.append(line1Entry)
-    
-    line2Label = tk.Label(newPagecanvas, text="Enter the second line of the speaker:")
-    line2Label.grid()
-    line2Entry = tk.Entry(newPagecanvas, width=100)
-    line2Entry.grid()
-    pageInfo.append(line2Entry)
-    
-    speakerLabel = tk.Label(newPagecanvas, text="Enter the speaker id:")
-    speakerLabel.grid()
-    speakerEntry = tk.Entry(newPagecanvas)
-    speakerEntry.grid()
-    pageInfo.append(speakerEntry)
-    
-    receiverLabel = tk.Label(newPagecanvas, text="Enter the receiver id:")
-    receiverLabel.grid()
-    receiverEntry = tk.Entry(newPagecanvas)
-    receiverEntry.grid()
-    pageInfo.append(receiverEntry)
-    
-    tauntLabel = tk.Label(newPagecanvas, text="Enter the taunt number (0 for none):")
-    tauntLabel.grid()
-    tauntEntry = tk.Entry(newPagecanvas)
-    tauntEntry.grid()
-    pageInfo.append(tauntEntry)
-    
-    demeanorLabel = tk.Label(newPagecanvas, text="Is the speaker happy (H) or angry (A)? (enter 0 for none): ")
-    demeanorLabel.grid()
-    demeanorEntry = tk.Entry(newPagecanvas)
-    demeanorEntry.grid()
-    pageInfo.append(demeanorEntry)
-    pages.append(pageInfo)
-    
-    separatorLabel = tk.Label(newPagecanvas, text="--------------------------------------------------------------------------------------------------")
-    separatorLabel.grid()
-    newPagecanvas.grid()
-
-
-def nextStepClicked(nextStep,frame,errorLabel,baseInfo):
-    errorLabel.grid_forget()
-    try:
-        global title
-        title = baseInfo[0].get()
-        if title == "":
-            raise Exception("No title")
-        global description
-        description = baseInfo[1].get()
-        if description == "":
-            raise Exception("No description")
-        if baseInfo[2].get() == "":
-            raise Exception("No wrestlers")
-        global wrestlers
-        wrestlers = int(baseInfo[2].get())
-        global team1
-        team1 = baseInfo[3].get()
-        global team2
-        team2 = baseInfo[4].get()
-        global ref
-        ref = baseInfo[5].get()
-        global total
-        total = wrestlers + team1 + team2 + ref
-        
-        for i in range(wrestlers):
-            tk.Label(master=frame,text=f"Wrestler {i+1} id: {i+1}").grid()
-        if team1 == 1:
-            tk.Label(master=frame,text=f"Wrestler 1 team member id: {wrestlers + team1}").grid()
-        if team2 == 1:
-            tk.Label(master=frame,text=f"Wrestler 2 team member id: {wrestlers + team1 + team2}").grid()
-        if ref == 1:
-            tk.Label(master=frame,text=f"Ref id: {wrestlers + team1 + team2 + ref}").grid()
-        baseInfo[0].config(state="disabled")
-        baseInfo[1].config(state="disabled")
-        baseInfo[2].config(state="disabled")
-        baseInfo[6].config(state="disabled")
-        baseInfo[7].config(state="disabled")
-        baseInfo[8].config(state="disabled")
-        newPageButton = tk.Button(frame, text="Create new page", command=lambda:[newPage(frame)])
-        newPageButton.grid()
-        createPromoButton = tk.Button(frame, text="Create .promo file", command=lambda:[formatPromo(newPageButton,createPromoButton)])
-        createPromoButton.grid()
-        nextStep.grid_forget()
-    except:
-        traceback.print_exc()
-        errorLabel.grid()
-
-
-
-def main(frame): 
-        
-    titleLabel = tk.Label(frame,text="Please enter the promo title:")
-    titleLabel.grid()
-    titleEntry = tk.Entry(frame)
-    titleEntry.grid()
-    baseInfo.append(titleEntry)
-    descLabel = tk.Label(frame,text="Please enter the promo description: (You can use '[P1]' and '[P2]' to name wrestlers)")
-    descLabel.grid()
-    descEntry = tk.Entry(frame,width=100)
-    descEntry.grid()
-    baseInfo.append(descEntry)
-    wrestlersLabel = tk.Label(frame,text="How many speakers are there? Min 0, max 3, not counting the ref or team partners:")
-    wrestlersLabel.grid()
-    wrestlersEntry = tk.Entry(master=frame)
-    wrestlersEntry.grid()
-    baseInfo.append(wrestlersEntry)
-
-    team1Var = tk.IntVar()
-    team2Var = tk.IntVar()
-    refVar = tk.IntVar()
-    team1Check = tk.Checkbutton(frame,text="Is the wrestler 1 team partner included in the promo?", variable=team1Var, onvalue=1, offvalue=0)
-    team1Check.grid()
-    baseInfo.append(team1Var)
-    team2Check = tk.Checkbutton(frame,text="Is the wrestler 2 team partner included in the promo?", variable=team2Var, onvalue=1, offvalue=0)
-    team2Check.grid()
-    baseInfo.append(team2Var)
-    refCheck = tk.Checkbutton(frame,text="Is the ref included in the promo?", variable=refVar, onvalue=1, offvalue=0)
-    refCheck.grid()
-    baseInfo.append(refVar)
-    baseInfo.append(team1Check)
-    baseInfo.append(team2Check)
-    baseInfo.append(refCheck)
-
-    errorLabel = tk.Label(frame,text="An error has occured, check if everything is correct and try again!")
-    nextStep = tk.Button(frame, text="Next step", command=lambda:[nextStepClicked(nextStep,frame,errorLabel,baseInfo)])
-    nextStep.grid()
-    
-
-def onFrameConfigure(canvas):
-    '''Reset the scroll region to encompass the inner frame'''
-    canvas.configure(scrollregion=canvas.bbox("all"))
-
-root = tk.Tk()
-root.title("Wrestling Empire Custom Promo Generator")
-root.geometry("800x800")
-canvas = tk.Canvas(root, borderwidth=0)
-frame = tk.Frame(canvas)
-vsb = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
-canvas.configure(yscrollcommand=vsb.set)
-
-vsb.pack(side="right", fill="y")
-canvas.pack(side="left", fill="both", expand=True)
-canvas.create_window((4,4), window=frame, anchor="nw")
-
-frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
-
-main(frame)
 
 root.mainloop()
